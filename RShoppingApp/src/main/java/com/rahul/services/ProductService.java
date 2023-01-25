eff19326256ad65705303d4075813f08242c1b9d
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-01-26 00:00:48 +0530

    updated

--- RShoppingApp/src/main/java/com/rahul/services/ProductService.java
+++ RShoppingApp/src/main/java/com/rahul/services/ProductService.java
@@ -1,10 +1,12 @@
 package com.rahul.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rahul.execptions.ProductException;
 import com.rahul.model.Product;
 import com.rahul.repository.ProductRepository;
 
@@ -14,23 +16,29 @@ public class ProductService {
  @Autowired 
  private ProductRepository productRepository; 
  
- public List<Product> getAllProducts() { 
-  return productRepository.findAll(); 
+ public List<Product> getAllProducts() throws ProductException { 
+  List<Product> allProducts= productRepository.findAll(); 
+  if(allProducts.size()==0) {
+	  throw new ProductException("Product is not available");
+  }
+  return allProducts;
  } 
  
- public Product getProductById(Long id) { 
-  return productRepository.findById(id).orElse(null); 
+ public Product getProductById(Long id) throws ProductException { 
+  Optional<Product> product = productRepository.findById(id); 
+  if(product.isEmpty()) {
+	  throw new ProductException("Invaild product Id");
+  }
+  return product.get();
  } 
  
  public Product createProduct(Product product) { 
   return productRepository.save(product); 
  } 
  
- public Product updateProduct(Long id, Product product) { 
-  Product existingProduct = productRepository.findById(id).orElse(null); 
-  if (existingProduct == null) { 
-   return null; 
-  } 
+ public Product updateProduct(Long id, Product product) throws ProductException { 
+  Product existingProduct  = getProductById(id);
+   
   existingProduct.setName(product.getName()); 
   existingProduct.setPrice(product.getPrice()); 
   existingProduct.setDescription(product.getDescription()); 
@@ -40,7 +48,8 @@ public class ProductService {
   return productRepository.save(existingProduct); 
  } 
  
- public void deleteProduct(Long id) { 
-  productRepository.deleteById(id); 
+ public String deleteProduct(Long id) throws ProductException { 
+	Product product=  getProductById(id);
+  productRepository.delete(product);
  } 
 } 
