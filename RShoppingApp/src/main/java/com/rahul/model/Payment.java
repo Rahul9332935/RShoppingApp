4ab46139c47d192dced9497c718adde1f7c8a554
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-02-03 23:57:16 +0530

    paymentService

--- RShoppingApp/src/main/java/com/rahul/model/Payment.java
+++ RShoppingApp/src/main/java/com/rahul/model/Payment.java
@@ -6,8 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.Data;
 
 @Entity 
+@Data
 public class Payment { 
  @Id 
  @GeneratedValue(strategy = GenerationType.IDENTITY) 
