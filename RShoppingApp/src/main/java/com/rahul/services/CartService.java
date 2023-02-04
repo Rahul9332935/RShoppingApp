f49f763444e82aeb792dae6ed13bd6f243395663
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-02-04 23:51:55 +0530

    c

--- RShoppingApp/src/main/java/com/rahul/services/CartService.java
+++ RShoppingApp/src/main/java/com/rahul/services/CartService.java
@@ -2,41 +2,18 @@ package com.rahul.services;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.rahul.model.Cart;
-import com.rahul.repository.CartRepository;
 
-@Service 
-public class CartService { 
- 
- @Autowired 
- private CartRepository cartRepository; 
- 
- public List<Cart> getAllCarts() { 
-  return cartRepository.findAll(); 
- } 
- 
- public Cart getCartById(Long id) { 
-  return cartRepository.findById(id).orElse(null); 
- } 
- 
- public Cart createCart(Cart cart) { 
-  return cartRepository.save(cart); 
- } 
- 
- public Cart updateCart(Long id, Cart cart) { 
-  Cart existingCart = cartRepository.findById(id).orElse(null); 
-  if (existingCart == null) { 
-   return null; 
-  } 
-  existingCart.setCustomer(cart.getCustomer()); 
-  existingCart.setItems(cart.getItems()); 
-  return cartRepository.save(existingCart); 
- } 
- 
- public void deleteCart(Long id) { 
-  cartRepository.deleteById(id); 
- } 
-} 
+public interface CartService {
+	
+	 public List<Cart> getAllCarts();
+	
+	 public Cart getCartById(Long id);
+	 
+	 public Cart createCart(Cart cart);
+	 
+	 public Cart updateCart(Long id, Cart cart);
+	 
+	 public void deleteCart(Long id);
+
+}
