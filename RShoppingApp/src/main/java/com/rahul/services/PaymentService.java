a52062c8274522ee4c4790350e797fc4db23b33a
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-02-07 23:58:28 +0530

    paymentServices

--- RShoppingApp/src/main/java/com/rahul/services/PaymentService.java
+++ RShoppingApp/src/main/java/com/rahul/services/PaymentService.java
@@ -2,42 +2,19 @@ package com.rahul.services;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.rahul.model.Payment;
-import com.rahul.repository.PaymentRepository;
 
-@Service 
-public class PaymentService { 
+public interface PaymentService {
 
-@Autowired 
-private PaymentRepository paymentRepository; 
-
-public List<Payment> getAllPayments() { 
-	  return paymentRepository.findAll(); 
-	 } 
-	 
-	 public Payment getPaymentById(Long id) { 
-	  return paymentRepository.findById(id).orElse(null); 
-	 } 
-	 
-	 public Payment createPayment(Payment payment) { 
-	  return paymentRepository.save(payment); 
-	 } 
-	 
-	 public Payment updatePayment(Long id, Payment payment) { 
-	  Payment existingPayment = paymentRepository.findById(id).orElse(null); 
-	  if (existingPayment == null) { 
-	   return null; 
-	  } 
-	  existingPayment.setOrder(payment.getOrder()); 
-	  existingPayment.setMethod(payment.getMethod()); 
-	  existingPayment.setDetails(payment.getDetails()); 
-	  return paymentRepository.save(existingPayment); 
-	 } 
-	 
-	 public void deletePayment(Long id) { 
-	  paymentRepository.deleteById(id); 
-	 } 
-	}
+	public List<Payment> getAllPayments();
+	
+	public Payment getPaymentById(Long id);
+	
+	public Payment createPayment(Payment payment) ;
+	
+	public Payment updatePayment(Long id, Payment payment);
+	
+	 public void deletePayment(Long id);
+	
+	
+}
