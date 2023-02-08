b9c65e92137d4f32b788942430882cff3b6b2eb3
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-02-08 23:46:36 +0530

    s

--- RShoppingApp/src/main/java/com/rahul/services/ProductService.java
+++ RShoppingApp/src/main/java/com/rahul/services/ProductService.java
@@ -1,56 +1,18 @@
 package com.rahul.services;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.rahul.model.Payment;
 
-import com.rahul.execptions.ProductException;
-import com.rahul.model.Product;
-import com.rahul.repository.ProductRepository;
+public interface ProductService {
 
-@Service 
-public class ProductService { 
- 
- @Autowired 
- private ProductRepository productRepository; 
- 
- public List<Product> getAllProducts() throws ProductException { 
-  List<Product> allProducts= productRepository.findAll(); 
-  if(allProducts.size()==0) {
-	  throw new ProductException("Product is not available");
-  }
-  return allProducts;
- } 
- 
- public Product getProductById(Long id) throws ProductException { 
-  Optional<Product> product = productRepository.findById(id); 
-  if(product.isEmpty()) {
-	  throw new ProductException("Invaild product Id");
-  }
-  return product.get();
- } 
- 
- public Product createProduct(Product product) { 
-  return productRepository.save(product); 
- } 
- 
- public Product updateProduct(Long id, Product product) throws ProductException { 
-  Product existingProduct  = getProductById(id);
-   
-  existingProduct.setName(product.getName()); 
-  existingProduct.setPrice(product.getPrice()); 
-  existingProduct.setDescription(product.getDescription()); 
-  existingProduct.setImages(product.getImages()); 
-  existingProduct.setAvailable(product.isAvailable()); 
-  existingProduct.setShippingInfo(product.getShippingInfo()); 
-  return productRepository.save(existingProduct); 
- } 
- 
- public String deleteProduct(Long id) throws ProductException { 
-	Product product=  getProductById(id);
-	productRepository.delete(product);
-	return "Product with Id:-"+id+" is deleted";
- } 
-} 
+	public List<Payment> getAllPayments();
+	
+	public Payment getPaymentById(Long id);
+	
+	public Payment createPayment(Payment payment);
+	
+	public Payment updatePayment(Long id, Payment payment);
+	
+	public void deletePayment(Long id);
+}
