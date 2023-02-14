879d34914df5128341361c42c65b2ef14b4c715a
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-02-14 23:39:46 +0530

    orderService

--- RShoppingApp/src/main/java/com/rahul/services/OrderService.java
+++ RShoppingApp/src/main/java/com/rahul/services/OrderService.java
@@ -2,17 +2,18 @@ package com.rahul.services;
 
 import java.util.List;
 
+import com.rahul.execptions.OrderException;
 import com.rahul.model.Orders;
 
 public interface OrderService {
 	
-	public List<Orders> getAllOrders();
+	public List<Orders> getAllOrders() throws OrderException;
 	
-	public Orders getOrderById(Long id) ;
+	public Orders getOrderById(Long id) throws OrderException ;
 	
 	public Orders createOrder(Orders order);
 	
-	public Orders updateOrder(Long id, Orders order);
+	public Orders updateOrder(Long id, Orders order) throws OrderException;
 	
-	public void deleteOrder(Long id);
+	public void deleteOrder(Long id) throws OrderException;
 }
--- RShoppingApp/src/main/java/com/rahul/services/OrderServiceImpl.java
+++ RShoppingApp/src/main/java/com/rahul/services/OrderServiceImpl.java
@@ -1,10 +1,12 @@
 package com.rahul.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rahul.execptions.OrderException;
 import com.rahul.model.Orders;
 import com.rahul.repository.OrderRepository;
 
@@ -14,22 +16,30 @@ public class OrderServiceImpl implements OrderService {
 	@Autowired 
 	private OrderRepository orderRepository; 
 
-	public List<Orders> getAllOrders() { 
-		return orderRepository.findAll(); 
+	public List<Orders> getAllOrders() throws OrderException { 
+		List<Orders>orders= orderRepository.findAll(); 
+		if(orders.size()==0) {
+			throw new OrderException("No Data available");
+		}
+		return orders;
 	} 
 
-	public Orders getOrderById(Long id) { 
-		return orderRepository.findById(id).orElse(null); 
+	public Orders getOrderById(Long id) throws OrderException { 
+		Optional<Orders> op= orderRepository.findById(id);
+		if(op.isEmpty()) {
+			throw new OrderException("No Order available with this order ID:- "+id);
+		}
+		return op.get();
 	} 
 
 	public Orders createOrder(Orders order) { 
 		return orderRepository.save(order); 
 	} 
 
-	public Orders updateOrder(Long id, Orders order) { 
+	public Orders updateOrder(Long id, Orders order) throws OrderException { 
 		Orders existingOrder = orderRepository.findById(id).orElse(null); 
 		if (existingOrder == null) { 
-			return null; 
+			throw new OrderException("No Order available with this order ID:- "+id);
 		} 
 		existingOrder.setCustomer(order.getCustomer()); 
 		existingOrder.setItems(order.getItems()); 
@@ -37,7 +47,8 @@ public class OrderServiceImpl implements OrderService {
 		return orderRepository.save(existingOrder); 
 	} 
 
-	public void deleteOrder(Long id) { 
-		orderRepository.deleteById(id); 
+	public void deleteOrder(Long id) throws OrderException {
+		Orders order=getOrderById(id);
+		orderRepository.delete(order);
 	} 
 } 
