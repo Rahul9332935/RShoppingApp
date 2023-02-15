54cf0b5060c95c75e342fd24cdf56bf7f0df6928
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-02-15 23:57:20 +0530

    Payment

--- RShoppingApp/src/main/java/com/rahul/services/PaymentService.java
+++ RShoppingApp/src/main/java/com/rahul/services/PaymentService.java
@@ -2,19 +2,20 @@ package com.rahul.services;
 
 import java.util.List;
 
+import com.rahul.execptions.PaymentException;
 import com.rahul.model.Payment;
 
 public interface PaymentService {
 
-	public List<Payment> getAllPayments();
+	public List<Payment> getAllPayments() throws PaymentException;
 	
-	public Payment getPaymentById(Long id);
+	public Payment getPaymentById(Long id) throws PaymentException;
 	
 	public Payment createPayment(Payment payment) ;
 	
-	public Payment updatePayment(Long id, Payment payment);
+	public Payment updatePayment(Long id, Payment payment) throws PaymentException;
 	
-	 public void deletePayment(Long id);
+	 public void deletePayment(Long id) throws PaymentException;
 	
 	
 }
--- RShoppingApp/src/main/java/com/rahul/services/PaymentServiceImpl.java
+++ RShoppingApp/src/main/java/com/rahul/services/PaymentServiceImpl.java
@@ -1,10 +1,12 @@
 package com.rahul.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rahul.execptions.PaymentException;
 import com.rahul.model.Payment;
 import com.rahul.repository.PaymentRepository;
 
@@ -14,30 +16,37 @@ public class PaymentServiceImpl implements PaymentService {
 @Autowired 
 private PaymentRepository paymentRepository; 
 
-	 public List<Payment> getAllPayments() { 
-		return paymentRepository.findAll(); 
+	 public List<Payment> getAllPayments() throws PaymentException { 
+		List<Payment> payments= paymentRepository.findAll(); 
+		if(payments.size()==0) {
+			throw new PaymentException("No data available");
+		}
+		return payments;
 	 } 
 	 
-	 public Payment getPaymentById(Long id) { 
-		 return paymentRepository.findById(id).orElse(null); 
+	 public Payment getPaymentById(Long id) throws PaymentException { 
+		 Optional<Payment> op= paymentRepository.findById(id); 
+		 if(op.isEmpty()) {
+			 throw new PaymentException("No data available by this id :- "+id);
+		 }
+		 return op.get();
 	 } 
 	 
 	 public Payment createPayment(Payment payment) { 
 		 return paymentRepository.save(payment); 
 	 } 
 	 
-	 public Payment updatePayment(Long id, Payment payment) { 
-		 Payment existingPayment = paymentRepository.findById(id).orElse(null); 
-		 if (existingPayment == null) { 
-			 return null; 
-		 } 
+	 public Payment updatePayment(Long id, Payment payment) throws PaymentException { 
+		 Payment existingPayment = getPaymentById(id); 
+		  
 		 existingPayment.setOrder(payment.getOrder()); 
 		 existingPayment.setMethod(payment.getMethod()); 
 		 existingPayment.setDetails(payment.getDetails()); 
 		 return paymentRepository.save(existingPayment); 
 	 } 
 	 
-	 public void deletePayment(Long id) { 
-		 paymentRepository.deleteById(id); 
+	 public void deletePayment(Long id) throws PaymentException { 
+		 Payment existingPayment = getPaymentById(id); 
+		 paymentRepository.delete(existingPayment);
 	 } 
 	}
