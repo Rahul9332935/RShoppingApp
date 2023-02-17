e9a56b440aebe37dca9fc3d940935a71cc09a515
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-02-17 23:52:42 +0530

    updated

--- RShoppingApp/src/main/java/com/rahul/controller/CartController.java
+++ RShoppingApp/src/main/java/com/rahul/controller/CartController.java
@@ -3,42 +3,39 @@ package com.rahul.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rahul.execptions.CartException;
 import com.rahul.model.Cart;
 import com.rahul.services.CartService;
 @RestController
-public class CartController implements CartService {
+public class CartController {
 
 	@Autowired
 	private CartService cartService;
 	
-	@Override
-	public List<Cart> getAllCarts() throws CartException {
+	public ResponseEntity<List<Cart>> getAllCarts() throws CartException {
 		
-		return cartService.getAllCarts();
+		return new ResponseEntity<>(cartService.getAllCarts(), HttpStatus.OK);
 	}
 
-	@Override
-	public Cart getCartById(Long id) throws CartException {
+	public ResponseEntity<Cart> getCartById(Long id) throws CartException {
 		
-		return cartService.getCartById(id);
+		return new ResponseEntity<>(cartService.getCartById(id),HttpStatus.OK);
 	}
 
-	@Override
-	public Cart createCart(Cart cart) {
+	public ResponseEntity<Cart> createCart(Cart cart) {
 		// TODO Auto-generated method stub
-		return cartService.createCart(cart);
+		return new ResponseEntity<>(cartService.createCart(cart), HttpStatus.CREATED);
 	}
 
-	@Override
-	public Cart updateCart(Long id, Cart cart) throws CartException {
+	public ResponseEntity<Cart> updateCart(Long id, Cart cart) throws CartException {
 		// TODO Auto-generated method stub
-		return cartService.updateCart(id, cart);
+		return new ResponseEntity<>(cartService.updateCart(id, cart), HttpStatus.OK);
 	}
 
-	@Override
 	public void deleteCart(Long id) throws CartException {
 		cartService.deleteCart(id);
 		
