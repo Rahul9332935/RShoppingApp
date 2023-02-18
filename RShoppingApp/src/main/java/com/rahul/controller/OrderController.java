0811738c6e7afd27a68ff39eaed4f4204fe7913b
Rahul <88732123+Rahul9332935@users.noreply.github.com>
2023-02-18 23:57:14 +0530

    some update

---
+++ RShoppingApp/src/main/java/com/rahul/controller/OrderController.java
@@ -0,0 +1,48 @@
+package com.rahul.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.rahul.execptions.OrderException;
+import com.rahul.model.Orders;
+import com.rahul.services.OrderService;
+
+public class OrderController {
+
+	@Autowired
+	private OrderService orderService;
+	
+	
+	public ResponseEntity<List<Orders>> getAllOrders() throws OrderException {
+		List<Orders> orders= orderService.getAllOrders();
+		return new ResponseEntity<List<Orders>>(orders, HttpStatus.OK);
+	}
+
+	
+	public  ResponseEntity<Orders> getOrderById(Long id) throws OrderException {
+		Orders order= orderService.getOrderById(id);
+		return new ResponseEntity<Orders>(order, HttpStatus.OK);
+	}
+
+	
+	public ResponseEntity<Orders> createOrder(Orders order) {
+		Orders order1= orderService.createOrder(order);
+		return new ResponseEntity<Orders>(order1, HttpStatus.ACCEPTED);
+	}
+
+	
+	public ResponseEntity<Orders> updateOrder(Long id, Orders order) throws OrderException {
+		Orders order1= orderService.updateOrder(id, order);
+		return ew ResponseEntity<Orders>(order1, HttpStatus.ACCEPTED);
+	}
+
+	
+	public void deleteOrder(Long id) throws OrderException {
+		orderService.deleteOrder(id);
+		
+	}
+
+}
